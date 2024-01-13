db31af0c45eef872ca140640b86086084868ac47
AlbnPrnc777 <alben.prince777@gmail.com>
2024-01-13 15:23:40 +0200

    admin dashboard

---
+++ src/scenes/transactions/index.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { useGetTransactionsQuery } from "state/api";
+import Header from "components/Header";
+import { Box, useTheme } from "@mui/material";
+
+const Transactions = () => {
+  const theme = useTheme();
+  const [page, setPage] = useState(0);
+  const [pageSize, setPageSize] = useState(20);
+  const [sort, setSort] = useState({});
+  const [search, setSearch] = useState("");
+
+  const { data, isLoading } = useGetTransactionsQuery({
+    page,
+    pageSize,
+    sort: JSON.stringify(sort),
+    search,
+  });
+  console.log("data", data);
+  return <div>Transactions</div>;
+};
+
+export default Transactions;
